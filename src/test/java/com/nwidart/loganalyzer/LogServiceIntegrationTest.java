86b4a2804d74c18aed110b218ff3e8788eb992c6
nwidart <n.widart@gmail.com>
2025-11-10 07:27:16 +0100

    Remove unused import

--- src/test/java/com/nwidart/loganalyzer/LogServiceIntegrationTest.java
+++ src/test/java/com/nwidart/loganalyzer/LogServiceIntegrationTest.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 class LogServiceIntegrationTest {
